ae539e3729697cbe29a0b21976df43d1be45cdaa
florianliepe <florian.liepe@eraneos.com>
2025-09-16 22:24:26 +0200

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import EraneosAIMaturityScan from './EraneosAIMaturityScan';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <EraneosAIMaturityScan />
+  </React.StrictMode>
+);
