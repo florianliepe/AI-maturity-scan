57bf2fb46f6a5ad0897423ddf630822306cf8a12
florianliepe <florian.liepe@eraneos.com>
2025-09-17 15:31:54 +0200

    Update EraneosAIMaturityScan.js

--- src/EraneosAIMaturityScan.js
+++ src/EraneosAIMaturityScan.js
@@ -12,7 +12,7 @@ import {
   BarElement,
 } from 'chart.js';
 import { Radar, Bar } from 'react-chartjs-2';
-import GitHubService from './GitHubService';
+import GitHubService from './GitHubServiceFixed';
 
 ChartJS.register(
   RadialLinearScale,
@@ -1353,3 +1353,4 @@ Assessment ID: ${assessmentData.id}
     </div>
   );
 }
+
