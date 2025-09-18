b317557606ad540196ab6bed42f7ba4268e4bf59
florianliepe <florian.liepe@eraneos.com>
2025-09-18 22:32:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import EraneosAIMaturityScan from './EraneosAIMaturityScan'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EraneosAIMaturityScan from './EraneosAIMaturityScan.js'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <EraneosAIMaturityScan />
-  </React.StrictMode>
-);
-
+  </React.StrictMode>,
+)
