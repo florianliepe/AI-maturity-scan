e1efe53bf5c24de931a322823fef392a4f4d5f7f
florianliepe <florian.liepe@eraneos.com>
2025-09-18 22:20:56 +0200

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'build',
+    assetsDir: 'static',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          charts: ['chart.js', 'react-chartjs-2']
+        }
+      }
+    }
+  },
+  define: {
+    'process.env': {}
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  preview: {
+    port: 3000
+  }
+})
