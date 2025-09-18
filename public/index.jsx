e145df21414921d2ef302e30251ef1434cfae41b
florianliepe <florian.liepe@eraneos.com>
2025-09-18 23:34:19 +0200

    Delete public directory

--- public/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import EraneosAIMaturityScan from './EraneosAIMaturityScan.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <EraneosAIMaturityScan />
-  </React.StrictMode>,
-)
